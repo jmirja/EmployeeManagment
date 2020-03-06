de2fb590d1c65f3690aa784bb9eb55a10669fce0
jmirja <41453618+jmirja@users.noreply.github.com>
2020-03-06 17:18:59 +0600

    Add project files.

---
+++ EmployeeManagment/Controllers/EmployeeController.cs
@@ -0,0 +1,122 @@
+﻿using EmployeeManagment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace EmployeeManagment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetView()
+        {
+            return View();
+        }
+
+        /// <summary>  
+        ///   
+        /// Get All Employee  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeDBEntities Obj = new EmployeeDBEntities())
+            {
+                List<Employee> Emp = Obj.Employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Employee With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (EmployeeDBEntities Obj = new EmployeeDBEntities())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.Employees.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Employee  
+        /// </summary>  
+        /// <param name="Employe"></param>  
+        /// <returns></returns>  
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeDBEntities Obj = new EmployeeDBEntities())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Delete Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeDBEntities Obj = new EmployeeDBEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Update Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeDBEntities Obj = new EmployeeDBEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employees.Where(x => x.ID == Emp.ID).FirstOrDefault();
+                    EmpObj.FirstName = Emp.FirstName;
+                    EmpObj.LastName = Emp.LastName;
+                    EmpObj.Gender = Emp.Gender;
+                    EmpObj.Salary = Emp.Salary;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ EmployeeManagment/Scripts/Controller/EmployeeCtrl.js
@@ -0,0 +1,81 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    debugger;
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employe = {};
+            $scope.Employe.FirstName = $scope.EmpFirstName;
+            $scope.Employe.LastName = $scope.EmpLastName;
+            $scope.Employe.Gender = $scope.EmpGender;
+            $scope.Employe.Salary = $scope.EmpSalary;
+            $http({
+                method: "post",
+                url: "https://localhost:44391/Employee/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpFirstName = "";
+                $scope.EmpLastName = "";
+                $scope.EmpGender = "";
+                $scope.EmpSalary = "";
+            })
+        } else {
+            $scope.Employe = {};
+            $scope.Employe.FirstName = $scope.EmpFirstName;
+            $scope.Employe.LastName = $scope.EmpLastName;
+            $scope.Employe.Gender = $scope.EmpGender;
+            $scope.Employe.Salary = $scope.EmpSalary;
+            $scope.Employe.ID = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "https://localhost:44391/Employee/Update_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpFirstName = "";
+                $scope.EmpLastName = "";
+                $scope.EmpGender = "";
+                $scope.EmpSalary = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "https://localhost:44391/Employee/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "https://localhost:44391/Employee/Delete_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("EmpID_").value = Emp.ID;
+        $scope.EmpFirstName = Emp.FirstName;
+        $scope.EmpLastName = Emp.LastName;
+        $scope.EmpGender = Emp.Gender;
+        $scope.EmpSalary = Emp.Salary;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Yellow";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})  
